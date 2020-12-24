73608979d4a4cd8cf5965f3c4966e85502689a2c
Marsel Serazetdinov <sermarsel@mail.ru>
2020-12-24 14:58:49 +0300

    Additional description.

--- src/itis/orders/OrderService.java
+++ src/itis/orders/OrderService.java
@@ -9,19 +9,20 @@ import java.util.ArrayList;
 //Сервис для работы с заказами. Менять интерфейс запрещено!
 public interface OrderService {
 
-	//Получить заказ по айди
+	//Получить заказ по айди, либо null если такого заказа не найдено
 	Order getOrderById(ArrayList<Order> orders, Long id);
 
-	//Получить список заказов в определенном статусе
+	//Получить список заказов в определенном статусе, либо пустой список если таких нет
 	ArrayList<Order> getAllOrdersByStatus(ArrayList<Order> orders, Status status);
 
-	//Получить список заказов со статусом определенной группы
+	//Получить список заказов со статусом определенной группы, либо пустой список если таких нет
 	ArrayList<Order> getAllOrdersByStatusGroup(ArrayList<Order> orders, StatusGroup statusGroup);
 
-	//Получить список заказов без удаленных позиций товаров
+	//Получить список заказов без удаленных позиций товаров, либо пустой список если таких нет
 	ArrayList<Order> getAllOrdersWithoutDeletedItems(ArrayList<Order> orders);
 
-	//Получить заказы, сумма которых больше заданной. Сумма заказов рассчитывается по следующей формуле:
+	//Получить заказы, сумма которых больше заданной, либо пустой список если таких нет.
+	// Сумма заказов рассчитывается по следующей формуле:
 	//Сумма произведений цены каждой позиции на ее количество (удаленные позиции не учитываются), с учетом скидки (в процентах)
 	ArrayList<Order> getOrdersWithPriceGreaterThan(ArrayList<Order> orders, Double price);
 
