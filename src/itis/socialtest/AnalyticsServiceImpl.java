1f436213eabe752d37a1e3ad25cfba0c0192c066
Zukhra Shagiakhmetova <zukhra.shagiakhmetova@gmail.com>
2021-04-17 12:09:37 +0300

    done

--- src/itis/socialtest/AnalyticsServiceImpl.java
+++ src/itis/socialtest/AnalyticsServiceImpl.java
@@ -2,26 +2,53 @@ package itis.socialtest;
 
 import itis.socialtest.entities.Post;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class AnalyticsServiceImpl implements AnalyticsService {
     @Override
     public List<Post> findPostsByDate(List<Post> posts, String date) {
-        return null;
+        return posts
+                .stream()
+                .filter(element -> element.getDate().split("T")[0].equals(date))
+                .collect(Collectors.toList());
     }
 
     @Override
     public String findMostPopularAuthorNickname(List<Post> posts) {
-        return null;
+        HashMap<String, Long> authors = new HashMap<>();
+        for (Post post : posts) {
+            if (authors.containsKey(post.getAuthor().getNickname())) {
+                authors.put(post.getAuthor().getNickname(), authors.get(post.getAuthor().getNickname()) + post.getLikesCount());
+            } else {
+                authors.put(post.getAuthor().getNickname(), post.getLikesCount());
+            }
+        }
+        long max = -1;
+        String nick = "";
+        for (Map.Entry<String, Long> entry : authors.entrySet()) {
+            if (entry.getValue() > max) {
+                nick = entry.getKey();
+            }
+        }
+        return nick;
     }
 
     @Override
     public Boolean checkPostsThatContainsSearchString(List<Post> posts, String searchString) {
-        return null;
+        return posts
+                .stream()
+                .anyMatch(element -> element.getContent().contains(searchString));
     }
 
     @Override
-    public List<Post> findAllPostsByAuthorNickname(List<Post> posts, String nick){
-        return null;
+    public List<Post> findAllPostsByAuthorNickname(List<Post> posts, String nick) {
+        return posts
+                .stream()
+                .filter(element -> element.getAuthor().getNickname().equals(nick))
+                .collect(Collectors.toList());
     }
+
 }
--- src/itis/socialtest/MainClass.java
+++ src/itis/socialtest/MainClass.java
@@ -1,8 +1,14 @@
 package itis.socialtest;
 
 
+import itis.socialtest.entities.Author;
 import itis.socialtest.entities.Post;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -36,11 +42,76 @@ public class MainClass {
     private AnalyticsService analyticsService = new AnalyticsServiceImpl();
 
     public static void main(String[] args) {
-        new MainClass().run("", "");
+        new MainClass().run("src/itis/socialtest/resources/PostDatabase.csv", "src/itis/socialtest/resources/Authors.csv");
     }
 
     private void run(String postsSourcePath, String authorsSourcePath) {
-
-
+        List<Author> authors = parseAuthors(authorsSourcePath);
+        List<Post> posts = parsePosts(postsSourcePath, authors);
+        // task 1
+        printPosts(posts);
+        AnalyticsService analyticsService = new AnalyticsServiceImpl();
+        // task 2
+        printPosts(analyticsService.findPostsByDate(posts, "17.04.2021"));
+        // task 3
+        printPosts(analyticsService.findAllPostsByAuthorNickname(posts, "varlamov"));
+        // task 4
+        System.out.println(analyticsService.checkPostsThatContainsSearchString(posts, "Россия"));
+        // task 5
+        System.out.println(analyticsService.findMostPopularAuthorNickname(posts));
     }
+
+    private List<Author> parseAuthors(String authorsSourcePath) {
+        List<Author> authors = new ArrayList<>();
+        try {
+            FileReader fileReader = new FileReader(new File(authorsSourcePath));
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] authorData = line.split(", ");
+                Long id = Long.parseLong(authorData[0]);
+                String nickname = authorData[1];
+                String birthdayDate = authorData[2];
+                authors.add(new Author(id, nickname, birthdayDate));
+            }
+            bufferedReader.close();
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return authors;
+    }
+
+    private List<Post> parsePosts(String postsSourcePath, List<Author> authors) {
+        List<Post> posts = new ArrayList<>();
+        try {
+            FileReader fileReader = new FileReader(new File(postsSourcePath));
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] postData = line.split(", ", 4);
+                String date = postData[2];
+                String content = postData[3];
+                Long likesCount = Long.parseLong(postData[1]);
+                Long authorId = Long.parseLong(postData[0]);
+                Author author = authors.stream().filter(element -> element.getId().equals(authorId)).findFirst().get();
+                posts.add(new Post(date, content, likesCount, author));
+            }
+            bufferedReader.close();
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    private void printPosts(List<Post> posts) {
+        for (Post post : posts) {
+            System.out.println("Date: " + post.getDate());
+            System.out.println("Likes: " + post.getLikesCount());
+            System.out.println("Author: " + post.getAuthor().getNickname());
+            System.out.println("Content: " + post.getContent() + "\n");
+        }
+    }
+
 }
